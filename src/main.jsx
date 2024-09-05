218e47c361dc63511b20a9319376e953285b717c
Akeem Mudashiru <110330636+Akeemmudash@users.noreply.github.com>
2024-09-05 13:39:08 +0100

    fixed build errors

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,7 @@ import "@fontsource/open-sauce-one";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
@@ -45,7 +41,11 @@ const router = createBrowserRouter([
       },
       {
         path: "contact",
-        element: <div><ContactUs /></div>,
+        element: (
+          <div>
+            <ContactUs />
+          </div>
+        ),
       },
       {
         path: "*", // This will match all undefined routes
